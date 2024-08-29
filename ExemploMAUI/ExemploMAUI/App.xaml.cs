992d6d05e8b74b2db5bbfe704f267e8764ecaf63
João Henrique <143840523+JoaoHCarvalho@users.noreply.github.com>
2024-08-28 21:12:05 -0300

    Main

---
+++ ExemploMAUI/ExemploMAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ExemploMAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
